23e133362f60df64a412624ab2f21a1c469c83f1
AugustoMoranUMG <gmroana2@miumg.edu.gt>
2026-05-18 23:38:14 -0600

    semana5

--- app.js
+++ app.js
@@ -4,13 +4,34 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var { connectDB } = require('./config/db');
+var initMysql = require("./config/initMysql");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tasksRouter = require('./routes/tasks');
 var goalsRouter = require('./routes/goals');
 
 var app = express();
+let mysqlDB = null;
 
+async function initializeDatabase() {
+  try {
+    if (process.env.DATABASE === 'MONGODB') {
+      await connectDB();
+      console.log("MongoDB conectado");
+    }
+
+    if (process.env.DATABASE === 'MYSQL') {
+      mysqlDB = await initMysql();
+      console.log("MySQL conectado y tablas verificadas");
+    }
+  } catch (error) {
+    console.error("Error inicializando base de datos:", error);
+  }
+}
+
+initializeDatabase();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   if ( req.headers.authorization && req.headers.authorization === '123456'){
+    req.db = mysqlDB;
     next();
   } else {
     res.status(401).json({ error: 'Unauthorized' });
@@ -32,7 +54,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
-app.use('/goals', tasksRouter);
+app.use('/goals', goalsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
